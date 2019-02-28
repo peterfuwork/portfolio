1d476b4709c3142a1718cad8def197d7a6ff00c1
Chiaen Fu <peterfuwork@gmail.com>
2019-02-27 17:00:38 -0700

    updated nav

--- public/scripts/script.js
+++ public/scripts/script.js
@@ -4,15 +4,20 @@ var body = $('body');
 var logo = $('.navs .nav a.logo');
 var topOfNav = nav.offset().top;
 var heightOfNav = nav.outerHeight();
+var navLinks = $('.navs nav .link');
 
 function fixNav() {
     if(window.scrollY >= topOfNav) {
         body.css('padding-top', heightOfNav + 'px');
         logo.css('transform','rotate(45deg)');
+        logo.css('max-width','12px');
+        navLinks.css('display','inline-block');
         body.addClass('fixed');
     } else {
         body.css('padding-top', 0);
         logo.css('transform','rotate(0deg)');
+        logo.css('max-width','50px');
+        navLinks.css('display','table-cell');
         body.removeClass('fixed');
     }
 }
